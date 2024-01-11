55a5fbb00ce8aea81f6c89158561d37bb33b64c6
sabur <sabur.yinus@gmail.com>
2024-01-12 00:13:42 +0100

    Selenium+java project

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String name = "Honda Suzuki";
+        int price = 2500000;
+        double weight = 46.85;
+        char number = 'E';
+        boolean availability = true;
+
+        System.out.println(name);
+        System.out.println(price);
+        System.out.println(weight);
+        System.out.println(number);
+        System.out.println(availability);
+    }
+}
